9709f5560cf326ebab914d6670f922de10e668b9
Mohamed Amine Bounab <mohamedaminebounab07@gmail.com>
2023-09-26 17:35:25 -0700

    alphabets in lower and upper cases

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+/**
+ *  main - entry point
+ *
+ *  Description: false or true validation
+ *
+ *  Return: 0 (success)
+*/
+
+int main(void)
+{
+	char ch = 'a';
+	char CH = 'A';
+
+	while (ch <= 'z')
+	{
+		putchar(ch);
+		ch++;
+	}
+	while (CH <= 'Z')
+	{
+		putchar(CH);
+		CH++;
+	}
+	putchar('\n');
+
+	return (0);
+}
+
