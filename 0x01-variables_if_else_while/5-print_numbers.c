9c3cb38adffbe3723b96c3290fe0fb32735a4e40
Mohamed Amine Bounab <mohamedaminebounab07@gmail.com>
2023-09-26 18:05:43 -0700

    numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ *  main - entry point
+ *
+ *  Description: false or true validation
+ *
+ *  Return: 0 (success)
+*/
+
+int main(void)
+{
+	int s = '0';
+
+	while (s => '0' && s <= '9')
+	{
+		printf("%d", s);
+		s++;
+	}
+	printf("\n");
+	return (0);
+}
+
+
